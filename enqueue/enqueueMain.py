db7ff40e039a672b9620a6c5bd47ab2883641111
Richard Mahn <rich.mahn@unfoldingword.org>
2023-10-19 12:12:52 -0600

    Better view

--- enqueue/enqueueMain.py
+++ enqueue/enqueueMain.py
@@ -762,7 +762,7 @@ def get_job_list_html(job_data):
     ref = job_data["ref"]
     event = job_data["event"]
 
-    html = f'ID:&nbsp;<a href="javascript:void(0)" onClick="$(\'#job-id\').val(\'{job_id}\');filterTable();return false;">{job_id[:5]}</a>&nbsp;<a href="job/{job_id}" title="View Job Info"><i class="fa-solid fa-info-circle"></i></a><br/>'+ \
+    html = f'ID:&nbsp;<a href="javascript:void(0)" onClick="$(\'#job-id\').val(\'{job_id}\');filterTable();return false;">{job_id.split("-")[0]}</a>&nbsp;<a href="job/{job_id}" title="View Job Info"><i class="fa-solid fa-info-circle"></i></a><br/>'+ \
             f'<div style="padding-left:2em; text-indent: -2em">Repo:&nbsp;<a href="javascript:void(0)" onClick="filterTable(\'{job_data["repo"].split("/")[0]}\')" title="Owner">'+ \
             f'<span class="text-nowrap">{job_data["repo"].split("/")[0]}</a></span> / '+ \
             f'<a href="javascript:void(0)" onClick="filterTable(\'{job_data["repo"]}\')" title="Repo">'+ \
